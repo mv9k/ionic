8e296629952dfb547d8e06725fd6cecf4103fff5
Kris <mv9kjw@gmail.com>
2016-02-03 09:46:05 -0700

    testing out ion-list

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -21,6 +21,23 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
+
+  .controller('CustomCtrl' , function($scope) {
+
+    $scope.items = [
+      { title: 'title', id: 1 },
+      { title: 'title', id: 2 },
+      { title: 'title', id: 3 },
+      { title: 'title', id: 4 },
+      { title: 'title', id: 5 },
+      { title: 'title', id: 6 },
+      { title: 'title', id: 7 },
+      { title: 'title', id: 8 }];
+
+  })
+
+
+
 .controller('AccountCtrl', function($scope) {
   $scope.settings = {
     enableFriends: true
